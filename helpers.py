79995bbf88a85282a4e130bc504ec45e5a08137a
Mustafa <musgamal05@gmail.com>
2022-05-22 10:37:13 +0200

    Prettifying the display of free time slots

--- helpers.py
+++ helpers.py
@@ -12,6 +12,46 @@ def timerange_to_minutes(t_Str):
 
     return hour_to_minutes + int(minutes_str)
 
-if __name__ == "__main__":
-    time_str = input("enter time string: ")
-    print(timerange_to_minutes(time_str))
+def minutes_to_timerange_str(m):
+    """
+    Return a timerange string in format of HH:MM for the given integer
+    :param m:
+    :return:
+    m = 90 -> 01:30
+    """
+    hour = m // 60
+    hour_str = f"{hour:02d}"
+    minutes = m % 60
+    minutes_str = f"{minutes:02d}"
+    return f"{hour_str}:{minutes_str}"
+
+def prettify_available_minutes(l: list):
+    grouped_list = []
+    list_resettable = []
+    # Group the list so that: [[0, 1, 2], [60,61, 62]]
+    for element in l:
+        if not list_resettable:
+            list_resettable.append(element)
+            continue
+        if list_resettable[-1] + 1 != element:
+            grouped_list.append(list_resettable[:])
+            list_resettable.clear()
+        list_resettable.append(element)
+    grouped_list.append(list_resettable)
+
+    time_ranges = []
+
+    for group in grouped_list:
+        start_time = minutes_to_timerange_str(m=group[0])
+        end_time = minutes_to_timerange_str(m=group[-1])
+        time_range_str = f"{start_time} - {end_time}"
+        time_ranges.append(time_range_str)
+
+    return time_ranges
+
+
+# if __name__ == "__main__":
+#     print(prettify_available_minutes())
+    # time_str = input("enter time string: ")
+    # print(timerange_to_minutes(time_str))
+    # print(minutes_to_timerange_str(m=120))
--- main.py
+++ main.py
@@ -1,11 +1,20 @@
+import helpers as h
 from TimeRange import TimeRange
 from Friend import Friend
 from custom_list import CustomList
 
 
+
 def main():
 
     available_minutes = CustomList(range(1440))
+    # print("Hello in Free Time Checker App")
+    # print("Here you can find the suitable time to hang out\nwith your friends besides your busy times")
+    # f_no = 1
+    # f_new = True
+    # while f_new == True
+    #     f = Friend(input(f"Please enter the name of Friend no. #{f_no}: "))
+    #     f.all_busy_minutes_range(TimeRange)
 
     f1 = Friend("Jim")
     f1.add_busy_range(TimeRange(start_time="08:00", end_time="10:00"))# 480 until 600 is not available
@@ -17,6 +26,7 @@ def main():
             if m in r:
                 available_minutes.remove_if_exist(m)
 
-    print(available_minutes)
+    for tr in h.prettify_available_minutes(available_minutes):
+        print(f"You can meet in {tr}")
 if __name__ == "__main__":
     main()
