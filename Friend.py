9fad7924e17eadffc7b1c3173d0e61b66c2bbaa4
Mustafa <musgamal05@gmail.com>
2022-05-22 09:07:22 +0200

    Calculating free time for more than one friend object

--- Friend.py
+++ Friend.py
@@ -1,15 +1,18 @@
 import helpers as h
 from TimeRange import TimeRange
 from dataclasses import dataclass, field
+from typing import ClassVar
 
 @dataclass
 class Friend:
+    all_busy_minutes_range: ClassVar[list] = []
     name: str
     busy_time_ranges: list[TimeRange] = field(default_factory=list, repr=False)
 
     def add_busy_range(self, obj:TimeRange):
         self.busy_time_ranges.append(obj)
-
+        # Add the minutes range object to a class attribute:
+        Friend.all_busy_minutes_range.append(obj.minutes_range)
 
 
 
--- main.py
+++ main.py
@@ -8,12 +8,13 @@ def main():
 
     f1 = Friend("Jim")
     f1.add_busy_range(TimeRange(start_time="08:00", end_time="10:00"))# 480 until 600 is not available
-    # f2 = Friend("Chris")
-    # f2.add_busy_range(TimeRange(start_time="12:00", end_time="14:00"))
+    f2 = Friend("Chris")
+    f2.add_busy_range(TimeRange(start_time="12:00", end_time="14:00"))# 720 until 840 is not available
 
     for m in available_minutes[:]:
-        if m in f1.busy_time_ranges[0].minutes_range:
-            available_minutes.remove(m)
+        for r in Friend.all_busy_minutes_range:
+            if m in r:
+                available_minutes.remove(m)
 
     print(available_minutes)
 if __name__ == "__main__":
